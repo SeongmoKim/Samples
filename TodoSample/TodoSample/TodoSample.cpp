7c2c2861d87faff658506767b79d3625d73f569a
Seongmo Kim <ringo6739@gmail.com>
2019-06-26 11:37:39 +0900

    #2 반복문 구현 수정 @SeongmoKim 수정했습니다.

--- TodoSample/TodoSample/TodoSample.cpp
+++ TodoSample/TodoSample/TodoSample.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
+#define FOR_STATEMENT_CYCLE 3
 int main(void)
 {
 	std::cout << "HelloWorld!" << std::endl;
 
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < FOR_STATEMENT_CYCLE; i++)
 	{
-		std::cout << "HelloGithub!" << std::endl;
+		if (i == 0)
+			std::cout << "Hello Github!" << std::endl;
+		else if (i == 1)
+			std::cout << "Hello C!" << std::endl;
+		else
+			std::cout << "Hello Java!" << std::endl;
 	}
 	return 0;
 }
